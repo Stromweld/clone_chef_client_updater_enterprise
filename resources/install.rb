963eb0b410d53c43bb9da56f52f12825b3f75e21
Corey Hemminger <hemminger@hotmail.com>
2026-09-16 09:08:31 -0500

    license key fix
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- resources/install.rb
+++ resources/install.rb
@@ -601,8 +601,9 @@ action :install do
       fresh_install = !chef_client_on_path?
       fresh_flag = fresh_install ? ' --fresh-install' : ''
       fstab_flag = fresh_install ? '' : " --fstab #{new_resource.fstab_handling}"
+      license_flag = new_resource.license_key ? " --license-key #{new_resource.license_key}" : ''
       "/hab/migration/bin/migrate-ice apply airgap #{bundle} --process-config ignore " \
-        "--license-key #{license}#{preserve_flag}#{fresh_flag}#{fstab_flag}"
+        "#{license_flag}#{preserve_flag}#{fresh_flag}#{fstab_flag}"
     }
     environment lazy { hab_env.merge('CHEF_LICENSE_KEY' => license.to_s) }
     sensitive true
